4c4f58d254be6cdffa94c6dc9fdfed64056f3833
Carlos <carlosempd@gmail.com>
2023-09-26 18:05:35 -0400

    🗃️ db: establish connection to mongo db atlas service

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
+const dbConfig = require('./config/connectDB')
 const productRoutes = require('./routes/productRoutes')
 
 app.use('/products', productRoutes)
+
+try {
+    dbConfig(process.env.MONGO_URL, process.env.DB_NAME)
+        .then(() => {
+            console.log('DB connection established');
+        }).catch(err => {
+            throw err
+        })
+} catch (error) {
+    throw new Error(`Database connection error: ${ error }`)
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
