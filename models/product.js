b60683e74ad348adfe0f372f8af17ad93fccd957
Carlos <carlosempd@gmail.com>
2023-09-26 19:06:34 -0400

    ✨ feat: created product mongoose schema

---
+++ models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    sku: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: String,
+        required: true
+    },
+})
+
+module.exports = mongoose.model('Product', productSchema)
